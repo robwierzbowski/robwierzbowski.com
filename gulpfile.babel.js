f26b432d4a39e2116d9ef48e3780707126496d4d
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2015-09-14 16:45:43 -0400

    Simplify serve task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -142,23 +142,18 @@ gulp.task('html', () => {
 // Clean output directory
 gulp.task('clean', cb => del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}, cb));
 
-// Watch files for changes & reload
+// Serve and Watch
 gulp.task('serve', ['styles'], () => {
   browserSync({
     notify: false,
-    // Customize the BrowserSync console logging prefix
-    logPrefix: 'WSK',
-    // Run as an https by uncommenting 'https: true'
-    // Note: this uses an unsigned certificate which on first access
-    //       will present a certificate warning in the browser.
-    // https: true,
+    logPrefix: 'BrowserSync',
     server: ['.tmp', 'app']
   });
 
-  gulp.watch(['app/**/*.html'], reload);
-  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
-  gulp.watch(['app/scripts/**/*.js'], ['jshint']);
-  gulp.watch(['app/images/**/*'], reload);
+  gulp.watch('app/**/*.html', reload);
+  gulp.watch('app/styles/**/*.scss', ['styles', reload]);
+  gulp.watch('app/scripts/**/*.js', ['jshint']);
+  gulp.watch('app/images/**/*', reload);
 });
 
 // Build and serve the output from the dist build
