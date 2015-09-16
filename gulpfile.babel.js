112d80a5f574b82ae56098cf798068f9fe84565b
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2015-09-15 20:20:41 -0400

    Simplify browser support

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -27,17 +27,7 @@ gulp.task('jshint', () =>
 
 // Compile and automatically prefix stylesheets
 gulp.task('styles', () => {
-  const AUTOPREFIXER_BROWSERS = [
-    'ie >= 10',
-    'ie_mob >= 10',
-    'ff >= 30',
-    'chrome >= 34',
-    'safari >= 7',
-    'opera >= 23',
-    'ios >= 7',
-    'android >= 4.4',
-    'bb >= 10'
-  ];
+  const SUPPORTED_BROWSERS = ['last 2 versions', '> 5%'];
 
   // For best performance, don't add Sass partials to `gulp.src`
   return gulp.src('app/styles/**/*.scss')
@@ -46,7 +36,7 @@ gulp.task('styles', () => {
   .pipe($.sass({
     precision: 10
   }).on('error', $.sass.logError))
-  .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+  .pipe($.autoprefixer(SUPPORTED_BROWSERS))
   .pipe($.sourcemaps.write())
   .pipe(gulp.dest('.tmp/styles'))
 
