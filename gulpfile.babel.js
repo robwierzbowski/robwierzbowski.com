5e2ebda16240940ce6d691ea5f0d9535131c34e8
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2015-10-19 13:45:01 -0400

    Comment out all unused gulpfile tasks
    These will come back in when I'm building again, and I'd like them here for
    quick reference. BUT, I'm about to make a script task and can't have the names
    colliding.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -78,165 +78,165 @@ gulp.task('serve', ['styles', 'jade'], () => {
 // .pipe(gulp.dest('dist/styles'))
 // .pipe($.size({title: 'styles'}));
 
-// Optimize images
-gulp.task('images', () =>
-  gulp.src('app/images/**/*')
-  .pipe($.cache($.imagemin({
-    progressive: true,
-    interlaced: true
-  })))
-  .pipe(gulp.dest('dist/images'))
-  .pipe($.size({title: 'images'}))
-);
+// // Optimize images
+// gulp.task('images', () =>
+//   gulp.src('app/images/**/*')
+//   .pipe($.cache($.imagemin({
+//     progressive: true,
+//     interlaced: true
+//   })))
+//   .pipe(gulp.dest('dist/images'))
+//   .pipe($.size({title: 'images'}))
+// );
 
-// Copy all files at the root level (app)
-gulp.task('copy', () =>
-  gulp.src([
-    'app/*',
-    '!app/*.jade',
-    'node_modules/apache-server-configs/dist/.htaccess'
-  ], {
-    dot: true
-  })
-  .pipe(gulp.dest('dist'))
-  .pipe($.size({title: 'copy'}))
-);
+// // Copy all files at the root level (app)
+// gulp.task('copy', () =>
+//   gulp.src([
+//     'app/*',
+//     '!app/*.jade',
+//     'node_modules/apache-server-configs/dist/.htaccess'
+//   ], {
+//     dot: true
+//   })
+//   .pipe(gulp.dest('dist'))
+//   .pipe($.size({title: 'copy'}))
+// );
 
-// Copy web fonts to dist
-gulp.task('fonts', () =>
-  gulp.src(['app/fonts/**'])
-  .pipe(gulp.dest('dist/fonts'))
-  .pipe($.size({title: 'fonts'}))
-);
+// // Copy web fonts to dist
+// gulp.task('fonts', () =>
+//   gulp.src(['app/fonts/**'])
+//   .pipe(gulp.dest('dist/fonts'))
+//   .pipe($.size({title: 'fonts'}))
+// );
 
-// Concatenate and minify JavaScript
-gulp.task('scripts', () =>
-  gulp.src([
-    // TODO: Then should I remove useref entirely?
-    // Note: Since we are not using useref in the scripts build pipeline,
-    //       you need to explicitly list your scripts here in the right order
-    //       to be correctly concatenated
-    './app/scripts/main.js'
-  ])
-  .pipe($.concat('main.min.js'))
-  .pipe($.uglify({preserveComments: 'some'}))
-  // Output files
-  .pipe(gulp.dest('dist/scripts'))
-  .pipe($.size({title: 'scripts'}))
-);
+// // Concatenate and minify JavaScript
+// gulp.task('scripts', () =>
+//   gulp.src([
+//     // TODO: Then should I remove useref entirely?
+//     // Note: Since we are not using useref in the scripts build pipeline,
+//     //       you need to explicitly list your scripts here in the right order
+//     //       to be correctly concatenated
+//     './app/scripts/main.js'
+//   ])
+//   .pipe($.concat('main.min.js'))
+//   .pipe($.uglify({preserveComments: 'some'}))
+//   // Output files
+//   .pipe(gulp.dest('dist/scripts'))
+//   .pipe($.size({title: 'scripts'}))
+// );
 
-// Scan your HTML for assets & optimize them
-gulp.task('html', () => {
-  const assets = $.useref.assets({searchPath: '{.tmp,app}'});
+// // Scan your HTML for assets & optimize them
+// gulp.task('html', () => {
+//   const assets = $.useref.assets({searchPath: '{.tmp,app}'});
 
-  return gulp.src('app/**/*.html')
-  .pipe(assets)
-  // Remove any unused CSS
-  // Note: If not using the Style Guide, you can delete it from
-  //       the next line to only include styles your project uses.
-  .pipe($.if('*.css', $.uncss({
-    html: [
-      'app/index.html'
-    ],
-    // CSS Selectors for UnCSS to ignore
-    ignore: [
-      /.navdrawer-container.open/,
-      /.app-bar.open/
-    ]
-  })))
+//   return gulp.src('app/**/*.html')
+//   .pipe(assets)
+//   // Remove any unused CSS
+//   // Note: If not using the Style Guide, you can delete it from
+//   //       the next line to only include styles your project uses.
+//   .pipe($.if('*.css', $.uncss({
+//     html: [
+//       'app/index.html'
+//     ],
+//     // CSS Selectors for UnCSS to ignore
+//     ignore: [
+//       /.navdrawer-container.open/,
+//       /.app-bar.open/
+//     ]
+//   })))
 
-  // Concatenate and minify styles
-  // In case you are still using useref build blocks
-  .pipe($.if('*.css', $.minifyCss()))
-  .pipe(assets.restore())
-  .pipe($.useref())
+//   // Concatenate and minify styles
+//   // In case you are still using useref build blocks
+//   .pipe($.if('*.css', $.minifyCss()))
+//   .pipe(assets.restore())
+//   .pipe($.useref())
 
-  // Minify any HTML
-  .pipe($.if('*.html', $.minifyHtml()))
-  // Output files
-  .pipe(gulp.dest('dist'))
-  .pipe($.size({title: 'html'}));
-});
+//   // Minify any HTML
+//   .pipe($.if('*.html', $.minifyHtml()))
+//   // Output files
+//   .pipe(gulp.dest('dist'))
+//   .pipe($.size({title: 'html'}));
+// });
 
-// Clean output directory
-gulp.task('clean', (done) => {
-  del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}, done);
-});
+// // Clean output directory
+// gulp.task('clean', (done) => {
+//   del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}, done);
+// });
 
-// Build and serve the output from the dist build
-gulp.task('serve:dist', ['default'], () =>
-  browserSync({
-    notify: false,
-    logPrefix: 'WSK',
-    // Run as an https by uncommenting 'https: true'
-    // Note: this uses an unsigned certificate which on first access
-    //       will present a certificate warning in the browser.
-    // https: true,
-    server: 'dist',
-    baseDir: 'dist'
-  })
-);
+// // Build and serve the output from the dist build
+// gulp.task('serve:dist', ['default'], () =>
+//   browserSync({
+//     notify: false,
+//     logPrefix: 'WSK',
+//     // Run as an https by uncommenting 'https: true'
+//     // Note: this uses an unsigned certificate which on first access
+//     //       will present a certificate warning in the browser.
+//     // https: true,
+//     server: 'dist',
+//     baseDir: 'dist'
+//   })
+// );
 
-// Build production files, the default task
-gulp.task('default', ['clean'], (done) => {
-  runSequence(
-    'styles',
-    ['jshint', 'html', 'scripts', 'images', 'fonts', 'copy'],
-    'generate-service-worker',
-    done
-  );
-});
+// // Build production files, the default task
+// gulp.task('default', ['clean'], (done) => {
+//   runSequence(
+//     'styles',
+//     ['jshint', 'html', 'scripts', 'images', 'fonts', 'copy'],
+//     'generate-service-worker',
+//     done
+//   );
+// });
 
-// Run PageSpeed Insights
-gulp.task('pagespeed', (done) => {
-  // Update the below URL to the public URL of your site
-  pagespeed('example.com', {
-    strategy: 'mobile',
-    // By default we use the PageSpeed Insights free (no API key) tier.
-    // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
-    // key: 'YOUR_API_KEY'
-  }, done);
-});
+// // Run PageSpeed Insights
+// gulp.task('pagespeed', (done) => {
+//   // Update the below URL to the public URL of your site
+//   pagespeed('example.com', {
+//     strategy: 'mobile',
+//     // By default we use the PageSpeed Insights free (no API key) tier.
+//     // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
+//     // key: 'YOUR_API_KEY'
+//   }, done);
+// });
 
-// See http://www.html5rocks.com/en/tutorials/service-worker/introduction/ for
-// an in-depth explanation of what service workers are and why you should care.
-// Generate a service worker file that will provide offline functionality for
-// local resources. This should only be done for the 'dist' directory, to allow
-// live reload to work as expected when serving from the 'app' directory.
-gulp.task('generate-service-worker', (done) => {
-  const rootDir = 'dist';
+// // See http://www.html5rocks.com/en/tutorials/service-worker/introduction/ for
+// // an in-depth explanation of what service workers are and why you should care.
+// // Generate a service worker file that will provide offline functionality for
+// // local resources. This should only be done for the 'dist' directory, to allow
+// // live reload to work as expected when serving from the 'app' directory.
+// gulp.task('generate-service-worker', (done) => {
+//   const rootDir = 'dist';
 
-  swPrecache({
-    // Used to avoid cache conflicts when serving on localhost.
-    cacheId: pkg.name,
-    staticFileGlobs: [
-      `${rootDir}/fonts/**/*.woff`,
-      `${rootDir}/images/**/*`,
-      `${rootDir}/scripts/**/*.js`,
-      `${rootDir}/styles/**/*.css`,
-      `${rootDir}/*.{html,json}`
-    ],
+//   swPrecache({
+//     // Used to avoid cache conflicts when serving on localhost.
+//     cacheId: pkg.name,
+//     staticFileGlobs: [
+//       `${rootDir}/fonts/**/*.woff`,
+//       `${rootDir}/images/**/*`,
+//       `${rootDir}/scripts/**/*.js`,
+//       `${rootDir}/styles/**/*.css`,
+//       `${rootDir}/*.{html,json}`
+//     ],
 
-    // Translates a static file path to the relative URL that it's served from.
-    stripPrefix: path.join(rootDir, path.sep)
-  }, (err, swFileContents) => {
-    if (err) {
-      done(err);
-      return;
-    }
+//     // Translates a static file path to the relative URL that it's served from.
+//     stripPrefix: path.join(rootDir, path.sep)
+//   }, (err, swFileContents) => {
+//     if (err) {
+//       done(err);
+//       return;
+//     }
 
-    const filepath = path.join(rootDir, 'service-worker.js');
+//     const filepath = path.join(rootDir, 'service-worker.js');
 
-    fs.writeFile(filepath, swFileContents, (err) => {
-      if (err) {
-        done(err);
-        return;
-      }
+//     fs.writeFile(filepath, swFileContents, (err) => {
+//       if (err) {
+//         done(err);
+//         return;
+//       }
 
-      done();
-    });
-  });
-});
+//       done();
+//     });
+//   });
+// });
 
-// Load custom tasks from the `tasks` directory
-// try { require('require-dir')('tasks'); } catch (err) { console.error(err); }
+// // Load custom tasks from the `tasks` directory
+// // try { require('require-dir')('tasks'); } catch (err) { console.error(err); }
